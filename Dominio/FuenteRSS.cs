5d6b7d083780987e1752da5916c2dc00ea4d2615
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-08-22 02:18:10 -0300

    primer carga

---
+++ Dominio/FuenteRSS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class FuenteRSS: Fuente
+    {
+        private string iNombreFuente;
+
+        //obtener pTexto desde el RSS
+        public FuenteRSS(string pTexto, string pNombreFuente) : base(pTexto)
+        {
+            this.iNombreFuente = pNombreFuente;
+        }
+    }
+}
