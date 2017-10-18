5262d526fdf7a2a0da515d0fab81852d4f7a1cb2
Mauri Chamorro <Mauricio-Chamorro@hotmail.com>
2017-10-18 18:57:50 -0300

    Fin parte inicial

--- Persistencia/DAL/EntityFramework/DatabaseInitializationStrategy.cs
+++ Persistencia/DAL/EntityFramework/DatabaseInitializationStrategy.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using Persistencia.Dominio;
 
 namespace Persistencia.DAL.EntityFramework
 {
-    class DatabaseInitializationStrategy
+    class DatabaseInitializationStrategy: CreateDatabaseIfNotExists<DbContext>
     {
+        protected override void Seed(DbContext context)
+        {
+            //instanciar clases y relaciones
 
+            base.Seed(context);
+        }
     }
 }
