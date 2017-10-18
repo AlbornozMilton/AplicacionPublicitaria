7a9680cf5fb166284579570fa52ca446d04960e6
Milton <Milton@DESKTOP-P6G4O5Q>
2017-10-18 18:03:59 -0300

    Dominio cargado

--- Persistencia/Dominio/RangoFecha.cs
+++ Persistencia/Dominio/RangoFecha.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Persistencia
+namespace Persistencia.Dominio
 {
-    class GenericRepository
+    class RangoFecha
     {
     }
 }
