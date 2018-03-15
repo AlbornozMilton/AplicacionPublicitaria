0c57dc90c3ad7faef8cd1197a3a16283e0d54de1
Milton <Milton@DESKTOP-P6G4O5Q>
2018-03-15 11:00:54 -0300

    AutoMapper

---
+++ Dominio/AutoMapper/AutoMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using pers = Persistencia.Dominio;
+
+namespace Dominio.AutoMapper
+{
+    class AutoMapper
+    {
+        public static void Mapear()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Campania, pers.Campania>().ReverseMap();
+
+                //cfg.CreateMap<, pers.Domicilio>().ReverseMap();
+
+                //cfg.CreateMap<Ciudad, pers.Ciudad>().ReverseMap();
+            });
+        }
+    }
+}
