3bce31e401f04d4d34dfe47a67108c40ff811f52
MauriChamorro <mmchamoo@gmail.com>
2018-04-14 17:49:33 -0300

    Agregado NuGet: Common.Logging y Terradue.ServiceModel.Syndication

---
+++ Dominio/RSS/RssReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominio.RSS
+{
+	public class RssReader: IRssReader
+	{
+		public IEnumerable<RssItem> Read(String pUrl)
+		{
+			if (String.IsNullOrWhiteSpace(pUrl))
+			{
+				throw new ArgumentException("pUrl");
+			}
+
+			return this.Read(new Uri(pUrl));
+		}
+
+		public abstract IEnumerable<RssItem> Read(Uri pUrl);
+	}
+}
