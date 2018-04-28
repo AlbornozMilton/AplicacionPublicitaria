ac09dc5cd20d12427a98f8ca6cb37947ffd92993
MauriChamorro <mmchamoo@gmail.com>
2018-04-28 02:40:10 -0300

    Entidad Item en Persistencia

---
+++ Persistencia/Dominio/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Dominio
+{
+	public class Item
+	{
+		public int ItemId { get; set; }
+		public string Titulo { get; set; }
+		public string Texto { get; set; }
+		public DateTime Fecha { get; set; }
+
+		public int FuenteId { get; set; }
+		public Fuente Fuente { get; set; }
+	}
+}
