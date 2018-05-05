94ba863248ade8b367e779b09c33991b9756c0b1
Milton <Milton@DESKTOP-P6G4O5Q>
2018-05-05 01:11:02 -0300

    eliminar

--- UI/EliminarCampañia.Designer.cs
+++ UI/EliminarCampañia.Designer.cs
@@ -30,16 +30,15 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.btn_Aceptar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.dGV_Campanias = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.btn_Aceptar = new System.Windows.Forms.Button();
-            this.btn_Cancelar = new System.Windows.Forms.Button();
-            this.gB_Filtrar = new System.Windows.Forms.GroupBox();
             this.dtp_FechaDesde = new System.Windows.Forms.DateTimePicker();
             this.label_Hasta = new System.Windows.Forms.Label();
             this.label_Desde = new System.Windows.Forms.Label();
@@ -48,10 +47,68 @@
             this.Nombre = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btn_Filtrar = new System.Windows.Forms.PictureBox();
+            this.gB_Filtrar = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dGV_Campanias)).BeginInit();
-            this.gB_Filtrar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btn_Filtrar)).BeginInit();
+            this.gB_Filtrar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cancelar.Location = new System.Drawing.Point(782, 317);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(81, 33);
+            this.btn_Cancelar.TabIndex = 33;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Aceptar
+            // 
+            this.btn_Aceptar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Aceptar.Location = new System.Drawing.Point(679, 317);
+            this.btn_Aceptar.Name = "btn_Aceptar";
+            this.btn_Aceptar.Size = new System.Drawing.Size(81, 33);
+            this.btn_Aceptar.TabIndex = 32;
+            this.btn_Aceptar.Text = "Aceptar";
+            this.btn_Aceptar.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(800, -1);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 48);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "_";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(838, 15);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 32);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 18);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Eliminar Campaña";
             // 
             // dGV_Campanias
             // 
@@ -65,13 +122,13 @@
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dGV_Campanias.Location = new System.Drawing.Point(372, 45);
+            this.dGV_Campanias.Location = new System.Drawing.Point(381, 50);
             this.dGV_Campanias.Name = "dGV_Campanias";
             this.dGV_Campanias.ReadOnly = true;
             this.dGV_Campanias.RowHeadersVisible = false;
             this.dGV_Campanias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dGV_Campanias.Size = new System.Drawing.Size(482, 143);
-            this.dGV_Campanias.TabIndex = 0;
+            this.dGV_Campanias.TabIndex = 28;
             // 
             // Column1
             // 
@@ -99,86 +156,6 @@
             this.Column3.HeaderText = "Fecha Fin";
             this.Column3.Name = "Column3";
             this.Column3.ReadOnly = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(16, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 18);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Eliminar Campaña";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.Color.Transparent;
-            this.label9.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.Transparent;
-            this.label9.Location = new System.Drawing.Point(829, 10);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(31, 32);
-            this.label9.TabIndex = 23;
-            this.label9.Text = "X";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Transparent;
-            this.label8.Location = new System.Drawing.Point(791, -6);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(32, 48);
-            this.label8.TabIndex = 24;
-            this.label8.Text = "_";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
-            // 
-            // btn_Aceptar
-            // 
-            this.btn_Aceptar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Aceptar.Location = new System.Drawing.Point(670, 312);
-            this.btn_Aceptar.Name = "btn_Aceptar";
-            this.btn_Aceptar.Size = new System.Drawing.Size(81, 33);
-            this.btn_Aceptar.TabIndex = 25;
-            this.btn_Aceptar.Text = "Aceptar";
-            this.btn_Aceptar.UseVisualStyleBackColor = true;
-            this.btn_Aceptar.Click += new System.EventHandler(this.btn_Aceptar_Click);
-            // 
-            // btn_Cancelar
-            // 
-            this.btn_Cancelar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Cancelar.Location = new System.Drawing.Point(773, 312);
-            this.btn_Cancelar.Name = "btn_Cancelar";
-            this.btn_Cancelar.Size = new System.Drawing.Size(81, 33);
-            this.btn_Cancelar.TabIndex = 26;
-            this.btn_Cancelar.Text = "Cancelar";
-            this.btn_Cancelar.UseVisualStyleBackColor = true;
-            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
-            // 
-            // gB_Filtrar
-            // 
-            this.gB_Filtrar.Controls.Add(this.dtp_FechaDesde);
-            this.gB_Filtrar.Controls.Add(this.label_Hasta);
-            this.gB_Filtrar.Controls.Add(this.label_Desde);
-            this.gB_Filtrar.Controls.Add(this.dtp_FechaHasta);
-            this.gB_Filtrar.Controls.Add(this.RangoFecha);
-            this.gB_Filtrar.Controls.Add(this.Nombre);
-            this.gB_Filtrar.Controls.Add(this.textBox1);
-            this.gB_Filtrar.Controls.Add(this.btn_Filtrar);
-            this.gB_Filtrar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gB_Filtrar.ForeColor = System.Drawing.Color.White;
-            this.gB_Filtrar.Location = new System.Drawing.Point(19, 45);
-            this.gB_Filtrar.Name = "gB_Filtrar";
-            this.gB_Filtrar.Size = new System.Drawing.Size(334, 300);
-            this.gB_Filtrar.TabIndex = 27;
-            this.gB_Filtrar.TabStop = false;
-            this.gB_Filtrar.Text = "Filtrar Campañas";
             // 
             // dtp_FechaDesde
             // 
@@ -261,30 +238,48 @@
             this.btn_Filtrar.TabIndex = 26;
             this.btn_Filtrar.TabStop = false;
             // 
+            // gB_Filtrar
+            // 
+            this.gB_Filtrar.BackColor = System.Drawing.Color.Maroon;
+            this.gB_Filtrar.Controls.Add(this.dtp_FechaDesde);
+            this.gB_Filtrar.Controls.Add(this.label_Hasta);
+            this.gB_Filtrar.Controls.Add(this.label_Desde);
+            this.gB_Filtrar.Controls.Add(this.dtp_FechaHasta);
+            this.gB_Filtrar.Controls.Add(this.RangoFecha);
+            this.gB_Filtrar.Controls.Add(this.Nombre);
+            this.gB_Filtrar.Controls.Add(this.textBox1);
+            this.gB_Filtrar.Controls.Add(this.btn_Filtrar);
+            this.gB_Filtrar.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gB_Filtrar.ForeColor = System.Drawing.Color.White;
+            this.gB_Filtrar.Location = new System.Drawing.Point(28, 50);
+            this.gB_Filtrar.Name = "gB_Filtrar";
+            this.gB_Filtrar.Size = new System.Drawing.Size(334, 300);
+            this.gB_Filtrar.TabIndex = 34;
+            this.gB_Filtrar.TabStop = false;
+            this.gB_Filtrar.Text = "Filtrar Campañas";
+            // 
             // EliminarCampañia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Maroon;
             this.BackgroundImage = global::UI.Properties.Resources.FondoVentanas;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(874, 364);
-            this.Controls.Add(this.gB_Filtrar);
+            this.ClientSize = new System.Drawing.Size(892, 381);
             this.Controls.Add(this.btn_Cancelar);
             this.Controls.Add(this.btn_Aceptar);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dGV_Campanias);
+            this.Controls.Add(this.gB_Filtrar);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "EliminarCampañia";
             this.Text = "EliminarCampañia";
-            this.Load += new System.EventHandler(this.EliminarCampañia_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dGV_Campanias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_Filtrar)).EndInit();
             this.gB_Filtrar.ResumeLayout(false);
             this.gB_Filtrar.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btn_Filtrar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -292,23 +287,23 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dGV_Campanias;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button btn_Aceptar;
         private System.Windows.Forms.Button btn_Cancelar;
-        private System.Windows.Forms.GroupBox gB_Filtrar;
+        private System.Windows.Forms.Button btn_Aceptar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dGV_Campanias;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DateTimePicker dtp_FechaDesde;
+        private System.Windows.Forms.Label label_Hasta;
+        private System.Windows.Forms.Label label_Desde;
+        private System.Windows.Forms.DateTimePicker dtp_FechaHasta;
         private System.Windows.Forms.CheckBox RangoFecha;
         private System.Windows.Forms.CheckBox Nombre;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox btn_Filtrar;
-        private System.Windows.Forms.DateTimePicker dtp_FechaHasta;
-        private System.Windows.Forms.Label label_Hasta;
-        private System.Windows.Forms.Label label_Desde;
-        private System.Windows.Forms.DateTimePicker dtp_FechaDesde;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.GroupBox gB_Filtrar;
     }
 }
